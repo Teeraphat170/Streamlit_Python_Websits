e2f4531888923aab7e9d78692177ddf525e7ffe6
Teeraphat170 <77708618+Teeraphat170@users.noreply.github.com>
2023-04-19 17:33:57 +0700

    vdvd

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Data.MainFile import ReadCSV
+import Data.MainFile
 import numpy as np
 import pandas as pd
 import pickle
@@ -10,5 +10,5 @@ TestX = pd.read_csv('Data/Dataset/All134.csv')
 # load_clf = pickle.load(open('Data/Model/IsolationForest_7.pkl', 'rb'))
 # print(load_clf)
 
-Result,timeX,prediction_proba,prediction = ReadCSV(TestX)
+Result,timeX,prediction_proba,prediction = Data.MainFile.ReadCSV(TestX)
 print(Result,timeX,prediction_proba,prediction)
--- Data/Predict.py
+++ Data/Predict.py
@@ -1,21 +1,12 @@
-import pandas as pd
-import numpy as np
+
 import pickle
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.decomposition import PCA
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn.ensemble import IsolationForest
-from sklearn.svm import OneClassSVM
 from datetime import datetime
 
 def predict(TestX):
     time = datetime.today().strftime('%H:%M:%S')
-    # filename = filename
     df = TestX
-    # print(df)
 
-    #unsupervised IsolationForest
-    load_clf = pickle.load(open("Model/IsolationForest_7.pkl", "rb")) #---------------------ใช้อันนี้เป็นหลัก---------------------#
+    load_clf = pickle.load(open("Model/IsolationForest_7.pkl", "rb")) 
 
     # IsolationForest
     prediction = load_clf.predict(df) 
