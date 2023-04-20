7f61ad17ffa5bdbd8cbb39c76e14cd4add113224
Teeraphat170 <77708618+Teeraphat170@users.noreply.github.com>
2023-04-20 16:23:06 +0700

    tt

--- App.py
+++ App.py
@@ -15,25 +15,26 @@ import plotly.express as px
 
 
 data = pd.read_csv('Data/Dataset/TotalFile35_36.csv')
-data = data["level_2"].head(10)
-DataQ = pd.DataFrame(data)
+ForTest = data
+ForTest = ForTest["level_2"].head(10)
+DataQ = pd.DataFrame(ForTest)
 
 placeholder = st.empty()
 
-for seconds in range(200):
+for seconds in range(10):
 
     list = []
     i = 0
-    while i < len(data.index):
+    while i < len(ForTest.index):
         i = i + 1
         list.append(i)
-        X = pd.DataFrame(list,columns = ['X'])
+        X = pd.DataFrame(list,columns = ['X-axis'])
 
         result = pd.concat([X, DataQ], axis=1)
 
     with placeholder.container():
 
-        st.line_chart(result, x='X')
+        st.line_chart(result, x='X-axis')
         time.sleep(1)
 
 
@@ -46,6 +47,6 @@ for seconds in range(200):
 
 # st.write(data)
 
-Result,timeX,prediction_proba,prediction = ReadCSV(data)
+# Result,timeX,prediction_proba,prediction = ReadCSV(data)
 # print(Result,timeX,prediction_proba,prediction)
 
